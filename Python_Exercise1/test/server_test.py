a78657e5c8d9971ca339dec6db8e603f97443ce2
Dina Thomas <dinaelizathomas@gmail.com>
2018-05-23 21:52:23 +0100

    Add test for respone code

--- Python_Exercise1/test/server_test.py
+++ Python_Exercise1/test/server_test.py
@@ -9,9 +9,13 @@ from basic_test import names_and_notes
 class TestServerOperation(unittest.TestCase):
 
     def test_get_notes(self):
-        test_response1= requests.get('http://127.0.0.1:5000/geo_Notes/hub_ethernet_sniffer')
+        test_response1 = requests.get('http://127.0.0.1:5000/geo_Notes/hub_ethernet_sniffer')
         test_dict1 = test_response1.json()
         self.assertEqual(test_dict1['hub_ethernet_sniffer'] , names_and_notes['hub_ethernet_sniffer'])
+
+    def test_get_status_code(self):
+        test_response2= requests.get('http://127.0.0.1:5000/geo_Notes/ct')
+        self.assertEqual(test_response2.status_code, 200)
 
 
 if __name__ == '__main__':

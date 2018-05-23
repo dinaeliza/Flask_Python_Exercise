3fde64ebbc097b47756cb4e94edccd3544fbad8f
Dina Thomas <dinaelizathomas@gmail.com>
2018-05-23 23:42:27 +0100

    Enhance the test script to explore the result of verbs other than get

--- Python_Exercise1/test/server_test.py
+++ Python_Exercise1/test/server_test.py
@@ -14,9 +14,20 @@ class TestServerOperation(unittest.TestCase):
         self.assertEqual(test_dict1['hub_ethernet_sniffer'] , names_and_notes['hub_ethernet_sniffer'])
 
     def test_get_status_code(self):
-        test_response2= requests.get('http://127.0.0.1:5000/geo_Notes/ct')
+        test_response2 = requests.get('http://127.0.0.1:5000/geo_Notes/ct')
         self.assertEqual(test_response2.status_code, 200)
 
+    def test_post_status_code(self):
+        test_response3 = requests.post('http://127.0.0.1:5000/geo_Notes/ct')
+        self.assertEqual(test_response3.status_code, 405)
+    
+    def test_put_status_code(self):
+        test_response4 = requests.put('http://127.0.0.1:5000/geo_Notes/ct')
+        self.assertEqual(test_response4.status_code, 405)
+
+    def test_delete_status_code(self):
+        test_response5 = requests.delete('http://127.0.0.1:5000/geo_Notes/ct')
+        self.assertEqual(test_response5.status_code, 405)
 
 if __name__ == '__main__':
     unittest.main()
